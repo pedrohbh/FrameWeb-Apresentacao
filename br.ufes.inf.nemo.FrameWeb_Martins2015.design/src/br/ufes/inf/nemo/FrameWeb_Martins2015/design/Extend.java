4794c882d98045b46fcbe7b5e0bbf7701c7cdae2
Nemo-PC-25\Silas Campos <silas_campos_23@hotmail.com>
2016-11-17 14:56:48 -0200

    Update 17/11

--- br.ufes.inf.nemo.FrameWeb_Martins2015.design/src/br/ufes/inf/nemo/FrameWeb_Martins2015/design/Extend.java
+++ br.ufes.inf.nemo.FrameWeb_Martins2015.design/src/br/ufes/inf/nemo/FrameWeb_Martins2015/design/Extend.java
@@ -14,53 +14,58 @@ public class Extend {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	public String printParameters(EObject m) {
-		String result;
 		
-		/*
-		EAttribute eAttribute = eObject.eClass().getEIDAttribute();
-	    if (eAttribute == null) {
-	        return eObject.eClass().getName();
-	    } else {
-	        return eObject.eClass().getName() + ' ' + eObject.eGet(eAttribute);
-	    }*/
-		//name - m.eGet(name).toString();
+		String result = null;
 		
-		EStructuralFeature ownedElement = m.eClass().getEAllStructuralFeatures().get(2);
-		EStructuralFeature name = m.eClass().getEAllStructuralFeatures().get(5);
-		EStructuralFeature methodType = m.eClass().getEAllStructuralFeatures().get(45);
-		EStructuralFeature ownedParameter = m.eClass().getEAllStructuralFeatures().get(24);
+		EStructuralFeature ownedElement = m.eClass().getEStructuralFeature("ownedElement");
+		EStructuralFeature name = m.eClass().getEStructuralFeature("name");
+		EStructuralFeature methodType = null;
+		EStructuralFeature ownedParameter = null;
+		String methodType_value = null;
+		Object parametros;
 		
 		String name_value = m.eGet(name).toString();
-		String methodType_value = m.eGet(methodType).toString();
-		Object parametros = m.eGet(ownedElement);
-		
+			
 		result = name_value + " (";
 		
-		System.out.println(m.eClass().getEAllStructuralFeatures().toString());
-		
-		EcoreEList parameter_list = (EcoreEList) m.eGet(ownedParameter);
-
-		int i;
-		for(i=0;i<parameter_list.size();i++){
-			EObject p = (EObject) parameter_list.get(i);
-			//On Parameter
-			EStructuralFeature parameter_name_feature = p.eClass().getEAllAttributes().get(0);
-			String parameter_name_value = p.eGet(parameter_name_feature).toString();
-			EStructuralFeature parameter_type_feature = p.eClass().getEAllStructuralFeatures().get(10);
-			EObject parameter_type_object = (EObject) p.eGet(parameter_type_feature);
-			EStructuralFeature parameter_type_name_feature = parameter_type_object.eClass().getEAllAttributes().get(0);
-			String parameter_type_name_value = parameter_type_object.eGet(parameter_type_name_feature).toString();
+		if(m.eClass().getEStructuralFeature("ownedParameter") != null){ 
+			ownedParameter = m.eClass().getEStructuralFeature("ownedParameter");
+			EcoreEList parameter_list = (EcoreEList) m.eGet(ownedParameter);
 			
-			result = result + parameter_name_value + " : " + parameter_type_name_value;
-			if(i!=parameter_list.size()-1) result = result + ", ";
-		}
+			int i;
+			for(i=0;i<parameter_list.size();i++){
+				EObject p = (EObject) parameter_list.get(i);
+				//On Parameter
+				String Type = "void";
+				EStructuralFeature parameter_name_feature = p.eClass().getEAllAttributes().get(0);
+				String parameter_name_value = p.eGet(parameter_name_feature).toString();
+				if(p.eClass().getEStructuralFeature("type")!=null){
+					EStructuralFeature parameter_type_feature = p.eClass().getEStructuralFeature("type");
+					if(p.eGet(parameter_type_feature) != null){
+						EObject parameter_type_object = (EObject) p.eGet(parameter_type_feature);
+						EStructuralFeature parameter_type_name_feature = parameter_type_object.eClass().getEAllAttributes().get(0);
+						Type = parameter_type_object.eGet(parameter_type_name_feature).toString();
+					}
+				}
+				result = result + parameter_name_value + " : " + Type;
+				if(i!=parameter_list.size()-1) result = result + ", ";
+			}
+			}
 		
-		result = result + ") : " + methodType_value;
+		result = result + ")";
+		
+		if(m.eClass().getEStructuralFeature("methodType") != null){
+			result = result + " : ";
+			methodType = m.eClass().getEStructuralFeature("methodType");
+			methodType_value = m.eGet(methodType).toString();
+			result = result + methodType_value;
+			}else{
+				result = result + " : " + "void";
+			}
 		
 		return result;
 	}
-	
 	
 }
