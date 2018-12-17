0dfa30f48d6a07b8b9fe6230041c4d8d2e03fc5f
lucas <lucas@LUCAS-PC>
2018-12-17 19:08:02 -0200

    Correção de sincronização

---
+++ core/plugins/codegenerator/br.ufes.inf.nemo.frameweb.codegenerator/src/br/ufes/inf/nemo/frameweb/codegenerator/CodeGenerator.java
@@ -0,0 +1,43 @@
+package br.ufes.inf.nemo.frameweb.codegenerator;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.diagram.DSemanticDiagram;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+
+import br.ufes.inf.nemo.frameweb.codegenerator.entity.EntityModelCodeGenerator;
+import br.ufes.inf.nemo.frameweb.utils.ProjectUtils;
+
+public class CodeGenerator implements IExternalJavaAction {
+
+	@Override
+	public boolean canExecute(Collection<? extends EObject> selections) {
+		boolean canExecute = selections
+			.stream()
+			.anyMatch(DSemanticDiagram.class::isInstance);
+		
+		return canExecute;
+	}
+
+	@Override
+	public void execute(Collection<? extends EObject> selections, Map<String, Object> arg1) {
+		IProject project = ProjectUtils.getSelectedProject();
+		IFolder srcFolder = project.getFolder("src");
+		
+		MainProjectRepresentation representation = new MainProjectRepresentation(selections);
+		
+		if (representation.hasEntityModel()) {
+			EntityModelCodeGenerator entityModelCodeGenerator = new EntityModelCodeGenerator(
+					representation.getEntityModel(),
+					representation.getORMTemplate()
+			);
+			
+			entityModelCodeGenerator.generate(srcFolder);
+		}
+	}
+
+}
---
+++ core/plugins/codegenerator/br.ufes.inf.nemo.frameweb.codegenerator/src/br/ufes/inf/nemo/frameweb/codegenerator/entity/EntityClassCodeGenerator.java
@@ -0,0 +1,5 @@
+package br.ufes.inf.nemo.frameweb.codegenerator.entity;
+
+public class EntityClassCodeGenerator {
+
+}
