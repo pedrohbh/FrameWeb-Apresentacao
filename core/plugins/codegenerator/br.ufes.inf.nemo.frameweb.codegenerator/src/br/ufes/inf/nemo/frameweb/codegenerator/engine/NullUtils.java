26b03858ef81013f2355fb98337bf3f46df74bfc
lucas <lucas@10.0.0.107>
2019-01-24 22:07:27 -0200

    Criação do utilitário para verificação de objetos nulos no template

---
+++ core/plugins/codegenerator/br.ufes.inf.nemo.frameweb.codegenerator/src/br/ufes/inf/nemo/frameweb/codegenerator/engine/NullUtils.java
@@ -0,0 +1,13 @@
+package br.ufes.inf.nemo.frameweb.codegenerator.engine;
+
+public class NullUtils {
+	
+	public boolean isNull(Object object) {
+		return object == null;
+	}
+	
+	public boolean isNotNull(Object object) {
+		return object != null;
+	}
+	
+}
