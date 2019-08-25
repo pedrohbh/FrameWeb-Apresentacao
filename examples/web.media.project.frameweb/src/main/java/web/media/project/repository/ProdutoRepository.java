63aeddfaf78ff52a5689f4b1ad793a91167bd5f6
lucasrms <lucasribeiromsr@gmail.com>
2019-08-25 00:05:31 -0300

    Projetos de exemplo zipados, não consegui subir .project nem a pau...

--- examples/web.media.project.frameweb/src/main/java/web/media/project/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package web.media.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import web.media.project.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
